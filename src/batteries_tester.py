7f89a6755bbef1b64ede9a20323c122a0b0589d9
clement <clement.vial@gmail.com>
2019-08-10 21:42:10 +0200

    rewriting using the classes

--- src/batteries_tester.py
+++ src/batteries_tester.py
@@ -16,9 +16,6 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 import time
 
 
-def fct():
-    return 1
-
 def close_relay(batt_id, batt_infos):
     # ==== close the relay ====
     RELAY_GPIO = batt_infos[batt_id]["relay_gpio"]
@@ -76,14 +73,83 @@ def set_up_conf():
 
 
 class Battery:
-    def __init__(self, relay_gpio, mcp_pin0, mcp_pin1):
-        self.relay_gpio = relay_gpio
-        self.mcp_pin0 = mcp_pin0
-        self.mcp_pin1 = mcp_pin1
+    def __init__(self, tester_slot):
+        self.tester_slot = tester_slot
         self.voltages_history = []
 
 
+class BatteriesTester:
+
+    def __init__(self):
+        self.map_slots = {
+            1: {"relay_gpio": 5, "mcp_pin0": MCP.P0, "mcp_pin1": MCP.P1},
+            2: {"relay_gpio": 6, "mcp_pin0": MCP.P2, "mcp_pin1": MCP.P3},
+            3: {"relay_gpio": 13, "mcp_pin0": MCP.P4, "mcp_pin1": MCP.P5},
+            4: {"relay_gpio": 19, "mcp_pin0": MCP.P6, "mcp_pin1": MCP.P7}
+        }
+
+        GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
+
+        # ==== MCP3008 hardware SPI configuration ====
+        spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+        # create the cs (chip select)
+        cs = digitalio.DigitalInOut(board.CE0)
+        # create the mcp object (harware option)
+        self.mcp = MCP.MCP3008(spi, cs)
+
+    def close_relay(self, battery):
+        # ==== close the relay ====
+        RELAY_GPIO = self.map_slots[battery.tester_slot]['relay_gpio']
+        # GPIO Assign mode
+        GPIO.setup(RELAY_GPIO, GPIO.OUT)
+        # close the relay
+        GPIO.output(RELAY_GPIO, GPIO.LOW)
+        # we need some time between the actions of the different relays
+        time.sleep(0.5)
+        return
+
+    def open_relay(self, battery):
+        # ==== close the relay ====
+        RELAY_GPIO = self.map_slots[battery.tester_slot]['relay_gpio']
+        # GPIO Assign mode
+        GPIO.setup(RELAY_GPIO, GPIO.OUT)
+        # close the relay
+        GPIO.output(RELAY_GPIO, GPIO.HIGH)
+        # we need some time between the actions of the different relays
+        time.sleep(0.5)
+        return
+
+    def read_voltage(self, battery):
+        # create a differential ADC channel between Pin 0 and Pin 1
+        pin0 = self.map_slots[battery.tester_slot]['mcp_pin0']
+        pin1 = self.map_slots[battery.tester_slot]['mcp_pin0']
+        return AnalogIn(self.mcp, pin0, pin1).voltage
+
+
 if __name__ == "__main__":
+    nb_of_batteries = 4
+    batteries = [Battery(x) for x in range(1, nb_of_batteries + 1)]
+
+    batteries_tester = BatteriesTester()
+
+    # close all the relays
+    for battery in batteries:
+        batteries_tester.close_relay(battery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__2":
     mcp, batt_infos = set_up_conf()
 
     # ==== beginning of the capacity measure ====
