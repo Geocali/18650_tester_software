03f06c14d471cda1af71064e5cc740fa6cde0ceb
clement <clement.vial@gmail.com>
2019-09-15 23:52:30 +0200

    simplify the battery class

--- angularapp/src/app/battery.ts
+++ angularapp/src/app/battery.ts
@@ -1,16 +1,7 @@
 export class Battery {
-    public static fromJson(json: Object): Battery {
-        let battery = 
-            {
-                slot_id: 1,
-                voltage: 3.3,
-                testing: false
-            }
-        ;
-        return battery
-    }
 
-    constructor(public slot_id: number,
-                public voltage: number,
-                public testing: boolean) {}
+    slot_id: number
+    voltage: number
+    testing: boolean
+    
 }
