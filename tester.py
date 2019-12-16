f783009ca391ee9c09711d14299680d772ddc2b2
Clément Vial <clement.vial@gmail.com>
2019-12-16 16:34:35 +0100

    remove raspberry emulation

--- tester.py
+++ tester.py
@@ -9,92 +9,13 @@ import os.path
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-i = 0
-
-try:
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-    import busio
-    import digitalio
-    import board
-    import adafruit_mcp3xxx.mcp3008 as MCP
-    from adafruit_mcp3xxx.analog_in import AnalogIn
-    env = "rpy"
-except RuntimeError:
-    env = "not_rpy"
-
-    # we create dummy objects to allow testing
-
-    class MCP:
-        P0 = 0
-        P1 = 1
-        P2 = 2
-        P3 = 3
-        P4 = 4
-        P5 = 5
-        P6 = 6
-        P7 = 7
-
-        def MCP3008(spi, cs):
-            return None
-
-    class board:
-        SCK = "SCK"
-        MISO = "MISO"
-        MOSI = "MOSI"
-        CE0 = "CE0"
-
-    class busio:
-        def SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI):
-            return None
-
-    class digitalio:
-        def DigitalInOut(CE0):
-            return None
-
-    class GPIO:
-        OUT = "OUT"
-        HIGH = "HIGH"
-        LOW = "LOW"
-
-        def setup(a, b):
-            return None
-
-        def output(selfa, b):
-            return None
-
-    def AnalogIn(mcp, pin0, pin1):
-
-        slot_id = pin0 / 2 + 1
-        voltage_start = 0
-        delta = 0.01
-        if i == 0:
-            # the program is starting
-            if slot_id == 1:
-                voltage_start = 0
-            elif slot_id == 2:
-                voltage_start = 0
-            elif slot_id == 3:
-                voltage_start = 0
-            elif slot_id == 4:
-                voltage_start = 0
-        else:
-            if slot_id == 1:
-                voltage_start = 4.1 - delta * i
-            elif slot_id == 2:
-                voltage_start = 4.15 - delta * i
-            elif slot_id == 3:
-                voltage_start = 4.2 - delta * i
-            elif slot_id == 4:
-                voltage_start = 4.25 - delta * i
-
-        # infos for this slot and last testing session
-        class AnalogInclass:
-
-            voltage = voltage_start * 3.3 / 5
-
-        return AnalogInclass
-
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 def close_relay(slot_id, slot_infos):
     # ==== close the relay ====
