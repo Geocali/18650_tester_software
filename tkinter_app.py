18c3d5d8d69df72db09d515d63afbddb6631c43f
clement <clement.vial@gmail.com>
2020-04-04 20:04:57 +0200

    plot from csv files

--- tkinter_app.py
+++ tkinter_app.py
@@ -4,14 +4,15 @@ from tkinter import Canvas
 from tkinter import messagebox as msg
 
 import numpy as np
+import pandas as pd
+
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
 from matplotlib.widgets import Slider, Button, RadioButtons
 
 #----------------------------------------------------------
 
-class LukeOutline(tk.Tk):
+class TesterOutline(tk.Tk):
 
     #------------------------------------------------------
     def __init__(self):
@@ -19,7 +20,7 @@ class LukeOutline(tk.Tk):
         super().__init__()
 
         # Title and size of the window
-        self.title('Luke Outline')
+        self.title('18650 battery tester')
         self.geometry('600x400')
 
         # Create the drop down menus
@@ -57,7 +58,7 @@ class LukeOutline(tk.Tk):
         self.fig = Figure()
         graph = FigureCanvasTkAgg(self.fig,self.graph_tab)
         graph.get_tk_widget().pack(side='top',fill='both',expand=True)
-        EllipseSlider(self.fig)
+        MainGraph(self.fig)
 
     #------------------------------------------------------
     def quit(self):
@@ -87,93 +88,45 @@ class LukeOutline(tk.Tk):
 
 #----------------------------------------------------------
 
-class EllipseSlider():
+class MainGraph():
 
     #------------------------------------------------------
     def __init__(self,fig):
         self.fig = fig
 
-        # Initial values
-        self.u = 0.     #x-position of the center
-        self.v = 0.     #y-position of the center
-        self.a = 2.     #radius on the x-axis
-        self.b = 1.5    #radius on the y-axis
+        i = 1
+        df_values = pd.read_csv("output/2019-12-09 151236_1_3_1302mAh.csv")
+        self.l, = self.create_plot(df_values, i)
 
-        # Points to plot against
-        self.t = np.linspace(0, 2*np.pi, 100)
+        i = 2
+        df_values = pd.read_csv("output/2019-12-09 152422_2_5_1510mAh.csv")
+        self.l, = self.create_plot(df_values, i)
+
+        i = 3
+        df_values = pd.read_csv("output/2019-12-09 155111_4_4_1935mAh.csv")
+        self.l, = self.create_plot(df_values, i)
+
+        i = 4
+        df_values = pd.read_csv("output/2019-12-09 160424_3_4_2153mAh.csv")
+        self.l, = self.create_plot(df_values, i)
+
+
+    def create_plot(self, df_values, i):
 
         # Set up figure with centered axes and grid
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_aspect(aspect='equal')
-        self.ax.spines['left'].set_position('center')
-        self.ax.spines['bottom'].set_position('center')
-        self.ax.spines['right'].set_color('none')
-        self.ax.spines['top'].set_color('none')
-        self.ax.xaxis.set_ticks_position('bottom')
-        self.ax.yaxis.set_ticks_position('left')
-        self.ax.set_xlim(-2,2)
-        self.ax.set_ylim(-2,2)
+        self.ax = self.fig.add_subplot(2, 2, i)
+        
+        self.ax.set_ylim(2.8,4.3)
         self.ax.grid(color='lightgray',linestyle='--')
 
-        # Initial plot
-        self.l, = self.ax.plot(self.u+self.a*np.cos(self.t),
-            self.v+self.b*np.sin(self.t),'k')
+        # create time into minutes since beginning of test
+        x = (pd.to_datetime(df_values.time) - pd.to_datetime(df_values.time.iloc[0])).astype('timedelta64[m]')
+        y = df_values.voltage.values
+        return self.ax.plot(x, y, 'k')
 
-        # Slider setup
-        self.axcolor = 'lightgoldenrodyellow'
-        self.axb = self.fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=self.axcolor)
-        self.axa = self.fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=self.axcolor)
-
-        self.sb = Slider(self.axb, 'Y Radius', 0.1, 2.0, valinit=self.b)
-        self.sa = Slider(self.axa, 'X Radius', 0.1, 2.0, valinit=self.a)
-
-        # Call update as slider is changed
-        self.sb.on_changed(self.update)
-        self.sa.on_changed(self.update)
-
-        # Reset if reset button is pushed
-        self.resetax = self.fig.add_axes([0.8,0.025,0.1,0.04])
-        self.button = Button(self.resetax, 'Reset', color=self.axcolor, hovercolor='0.975')
-
-        self.button.on_clicked(self.reset)
-
-        # Color button setup
-        self.rax = self.fig.add_axes([0.025, 0.5, 0.15, 0.15], facecolor=self.axcolor)
-        self.radio = RadioButtons(self.rax, ('red', 'blue', 'green'), active=0)
-
-        self.radio.on_clicked(self.colorfunc)
-
-    #------------------------------------------------------
-
-    def update(self, val):
-        '''
-        Updates the plot as sliders are moved
-        '''
-        self.a = self.sa.val
-        self.b = self.sb.val
-        self.l.set_xdata(self.u+self.a*np.cos(self.t))
-        self.l.set_ydata(self.u+self.b*np.sin(self.t))
-
-    #------------------------------------------------------
-
-    def reset(self, event):
-        '''
-        Resets everything if reset button clicked
-        '''
-        self.sb.reset()
-        self.sa.reset()
-
-    #------------------------------------------------------
-
-    def colorfunc(self, label):
-        '''
-        Changes color of the plot when button clicked
-        '''
-        self.l.set_color(label)
-        self.fig.canvas.draw_idle()
 
 #----------------------------------------------------------
 
 if __name__ == '__main__':
-    luke_gui = LukeOutline()
-    luke_gui.mainloop()
+    tester_gui = TesterOutline()
+    tester_gui.mainloop()
